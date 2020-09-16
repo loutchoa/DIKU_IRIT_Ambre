05b11eed614c1a01802edd77fc674a8626b20aa0
jmelou <jeme@mikrosimage.eu>
2020-09-16 17:06:33 +0200

    Change main name

--- Script Punaise Sphere/main.m
+++ Script Punaise Sphere/main.m
@@ -38,53 +38,14 @@ Num_Img_Ref = 1 ;
 Liste_Num_Img_Ctrl = [] ;
 Liste_Num_Imgs_Temoins = [28, 29, 2, 3, 8] ;
 tic
-[Nuage_1, Couleur_1] = MVS_Boule(Num_Img_Ref , Liste_Num_Img_Ctrl, ...
+[Nuage, Couleur] = MVS_Boule(Num_Img_Ref , Liste_Num_Img_Ctrl, ...
     Liste_Num_Imgs_Temoins, Barycentre, Imgs, Masques_Imgs, Pts_Dioptres, ...
     Masques_Imgs_Projections_Pts_Dioptres, Imgs_2_Dioptres, Dioptres_2_Imgs, ...
     t, Nb_De_Tranches, n_Air, n_Verre, Taille_Fenetre_SAD, Profondeur) ;
 toc
-
-% MVS 2
-Num_Img_Ref = 8 ;
-Liste_Num_Img_Ctrl = [] ;
-Liste_Num_Imgs_Temoins = [6, 7, 9, 10, 1] ;
-tic
-[Nuage_2, Couleur_2] = MVS_Boule(Num_Img_Ref , Liste_Num_Img_Ctrl, ...
-    Liste_Num_Imgs_Temoins, Barycentre, Imgs, Masques_Imgs, Pts_Dioptres, ...
-    Masques_Imgs_Projections_Pts_Dioptres, Imgs_2_Dioptres, Dioptres_2_Imgs, ...
-    t, Nb_De_Tranches, n_Air, n_Verre, Taille_Fenetre_SAD, Profondeur) ;
-toc
-
-% MVS 3
-Num_Img_Ref = 16 ;
-Liste_Num_Img_Ctrl = [] ;
-Liste_Num_Imgs_Temoins = [14, 15, 17, 18, 22] ;
-tic
-[Nuage_3, Couleur_3] = MVS_Boule(Num_Img_Ref , Liste_Num_Img_Ctrl, ...
-    Liste_Num_Imgs_Temoins, Barycentre, Imgs, Masques_Imgs, Pts_Dioptres, ...
-    Masques_Imgs_Projections_Pts_Dioptres, Imgs_2_Dioptres, Dioptres_2_Imgs, ...
-    t, Nb_De_Tranches, n_Air, n_Verre, Taille_Fenetre_SAD, Profondeur) ;
-toc
-
-% MVS 4
-Num_Img_Ref = 22 ;
-Liste_Num_Img_Ctrl = [] ;
-Liste_Num_Imgs_Temoins = [20, 21, 23, 24, 16] ;
-tic
-[Nuage_4, Couleur_4] = MVS_Boule(Num_Img_Ref , Liste_Num_Img_Ctrl, ...
-    Liste_Num_Imgs_Temoins, Barycentre, Imgs, Masques_Imgs, Pts_Dioptres, ...
-    Masques_Imgs_Projections_Pts_Dioptres, Imgs_2_Dioptres, Dioptres_2_Imgs, ...
-    t, Nb_De_Tranches, n_Air, n_Verre, Taille_Fenetre_SAD, Profondeur) ;
-toc
-
-
-Nuage = [Nuage_1 ; Nuage_2 ; Nuage_3 ; Nuage_4] ;
-Couleur = [Couleur_1 ; Couleur_2 ; Couleur_3 ; Couleur_4] ;
 
 % Affichage
 figure(1)
 nuage_points_3D = pointCloud(Nuage,'Color',uint8(Couleur));
 pcshow(nuage_points_3D,'VerticalAxis','y','VerticalAxisDir','down','MarkerSize',45);
 axis equal
-
-% save(Output_Name, 'Nuage_1', 'Nuage_2', 'Nuage_3', 'Nuage_4', 'Nuage', 'Couleur_1', 'Couleur_2', 'Couleur_3', 'Couleur_4', 'Couleur', 'nuage_points_3D') ;
