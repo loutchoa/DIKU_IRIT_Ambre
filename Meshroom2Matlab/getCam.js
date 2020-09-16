8287a616d23b295cb60a8e1f13829e7f4d3b88a8
MatthewCassidy <matthew.cassidy@etu.enseeiht.fr>
2020-09-16 16:22:38 +0200

    retrive all repositories

---
+++ Meshroom2Matlab/getCam.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var path2sfmFile = process.argv[2];
+
+var data = JSON.parse(fs.readFileSync(path2sfmFile,'utf-8'));
+
+var text = '';
+var f = data.intrinsics[0].pxFocalLength;
+var x0 = data.intrinsics[0].principalPoint[0];
+var y0 = data.intrinsics[0].principalPoint[1];
+
+text += 'K = [' + f +' 0 ' + x0 + ';\n 0 ' + f + ' ' + y0 + ';\n 0 0 1];\n\n'
+for (var extIndex = 0; extIndex<data.poses.length; extIndex++) {
+    var ext = data.poses[extIndex];
+    text += 'R(:,:,' + (extIndex + 1) + ') = [';
+    for (var rowIndex = 0; rowIndex < ext.pose.transform.rotation.length; rowIndex++) {
+        var row = ext.pose.transform.rotation[rowIndex];
+        if ((rowIndex + 1) % 3 != 0) {
+            text += row + ' ';
+        } else if((rowIndex + 1) == 9) {
+            text += row + '];\n\n';
+        } else {
+            text += row + ';\n';
+        }
+    }
+    text += 'T(:,' + (extIndex + 1) + ') = [';
+    text += ext.pose.transform.center.join(';') + '];\n\n';
+}
+
+console.log(text);
---
+++ Script Punaise Sphere/Calculer_Coord_Pts_Sphere.m
@@ -0,0 +1,9 @@
+function [Centre_Sphere, Coord_Pts_Sphere] = Calculer_Coord_Pts_Sphere(Nb_De_Faces)
+    [X,Y,Z] = sphere(Nb_De_Faces) ;
+    Rayon_Sphere = 15 ;
+    Centre_Sphere = [0 ; 0 ; 100] ;
+    X = X*Rayon_Sphere + Centre_Sphere(1) ;
+    Y = Y*Rayon_Sphere + Centre_Sphere(2) ;
+    Z = Z*Rayon_Sphere + Centre_Sphere(3) ;
+    Coord_Pts_Sphere = [X(:) Y(:) Z(:)] ;
+end
