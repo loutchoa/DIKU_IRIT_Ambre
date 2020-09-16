3eae4634b89421e0ad2fdcc6864fe055bd9826fd
jmelou <jeme@mikrosimage.eu>
2020-09-16 23:08:49 +0200

    [refactoring] Add interface structure

--- Script Punaise Sphere/main.m
+++ Script Punaise Sphere/main.m
@@ -1,16 +1,23 @@
 clear variables ;
 
 %%%%%% PARAMETRES %%%%%%%%
-Nb_De_Faces = 50 ;
+% Diopter's properties
+interface.shape = 'sphere';
+interface.facesNumber = 50;
+interface.radius = 10;
+interface.center = [0; 0; 100];
+
 Nb_De_Tranches = 50 ;
 Taille_Fenetre_SAD = 3 ; % 3*3
 Profondeur = 20 ;
-Output_Name = "Punaise_Sphere_" + int2str(Nb_De_Faces) + "_" + int2str(Nb_De_Tranches) + ".mat" ;
+Output_Name = "Punaise_Sphere_" + int2str(interface.facesNumber) + "_" + int2str(Nb_De_Tranches) + ".mat" ;
 
 %%%%%%%%% DONNEES %%%%%%%%%%%%%%
 
-% Echantillonage de la surface de l'ambre + Barycentre ;
-[Barycentre, Pts_Surface_Ambre] = Calculer_Coord_Pts_Sphere(Nb_De_Faces) ;
+%% Get interface points and normals :
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%[Barycentre, Pts_Surface_Ambre] = Calculer_Coord_Pts_Sphere(interface.facesNumber) ;
+[interface.points, interface.normals] = interfaceSampling(interface);
 
 load('Imgs_Et_Masques_Punaise_1.5.mat') ;
 load('Cameras.mat') ;
@@ -20,8 +27,9 @@ n_Air = 1 ; n_Verre = 1.5 ; % n_Ambre = 1.541 ;
 
 % Dioptre pour chaque Camera
 Nb_Imgs = size(Imgs, 4) ;
-[Pts_Dioptres, Booleen_Pts_Dioptres] = Calculer_Dioptres(...
+%[Pts_Dioptres, Booleen_Pts_Dioptres] = Calculer_Dioptres(...
     Position_Camera, Barycentre, Pts_Surface_Ambre) ;
+[Pts_Dioptres, Booleen_Pts_Dioptres] = Calculer_Dioptres(Position_Camera, interface.center, interface.points);
 
 % Matrice de calibrage K
 [Nb_De_Lignes, Nb_De_Colonnes, ~, ~] = size(Imgs) ;
