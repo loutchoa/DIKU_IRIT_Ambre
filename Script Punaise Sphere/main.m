d124c60eb5527c6f6517f3c55fdced977ff90e20
MatthewCassidy <matthew.cassidy@etu.enseeiht.fr>
2020-10-06 16:51:42 +0200

    Add function getStereoData

--- Script Punaise Sphere/main.m
+++ Script Punaise Sphere/main.m
@@ -49,39 +49,20 @@ data.Masques_Imgs = Masques_Imgs(:, :, usedCam);
 camera.R = R(:,:,usedCam);
 camera.t = t(usedCam,:);
 camera.K = evalK(nb_rows, nb_col, camera);
-
 [camera.visiblePoints, camera.interfacePoints2Pixels] = getCorrespondances(camera, interface, data);
 
 %% Prepare Stereo Data
 %% Pictures are "vectorized" and neighboring pixels are aligned along the 3rd dimension
-
-for picture = 1:nb_im
-	antiMask = find(data.Masques_Imgs(:,:,picture) == 0);
-	currentIm = data.Imgs(:,:,:,picture);
-	currentIm = reshape(currentIm, [nb_rows * nb_col, nb_ch]);
-	currentIm(antiMask, :) = 0;
-	currentIm = reshape(currentIm, [nb_rows, nb_col, nb_ch]);
-	imStereo = cat(4,...
-		currentIm([1 1:end-1],[1 1:end-1],:),...                        % Top left
-		currentIm([1 1:end-1],:,:),...                                  % Top
-		currentIm([1 1:end-1],[2:end end],:),...                        % Top right
-		currentIm(:,[1 1:end-1],:),...                                  % Left
-		currentIm,...                                                   % Center
-		currentIm(:,[2:end end],:),...                                  % Right
-		currentIm([2:end end],[1 1:end-1],:),...                        % Bottom left
-		currentIm([2:end end],:,:),...                                  % Bottom
-		currentIm([2:end end],[2:end end],:));                          % Bottom right
-	imStereo = reshape(imStereo,[nb_rows * nb_col, nb_ch,9]);
-    data.imStereo(:,:,:,picture) = permute(imStereo,[1,3,2]);
-end
+data.imStereo = getStereoData(data);
 
 
-% MVS
+
+%% MVS
 tic
 [Nuage, Couleur] = MVS_Boule(data, camera, interface, options, param);
 toc
 
-% Affichage
+%% Affichage
 figure(1)
 nuage_points_3D = pointCloud(Nuage,'Color',uint8(Couleur));
 pcshow(nuage_points_3D,'VerticalAxis','y','VerticalAxisDir','down','MarkerSize',45);
